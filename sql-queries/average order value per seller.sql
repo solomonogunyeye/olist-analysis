9fcea5ccf4a586ff8f2b471ae4fd5319c9ad2d17
Solomon Ayodele Ogunyeye <saolad1@hotmail.com>
2026-02-24 17:39:23 +0100

    Add files via upload

---
+++ sql-queries/average order value per seller.sql
@@ -0,0 +1,6 @@
+SELECT
+     seller_id,
+	 COUNT(DISTINCT order_date) AS active_days,
+	 ROUND( SUM(revenue) / COUNT(DISTINCT order_date), 2) AS avg_revenue_per_day
+FROM seller_performance_base
+GROUP BY seller_id;
